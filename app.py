73d55977efaec1483760d2e3cf0246892c0bd717
KellianT <kellianth@outlook.com>
2021-03-03 11:25:06 +0100

    Début structure + implémentation fonction ouvrir_csv

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
-
+from fonctions import ouvrir_csv
+from Flask import jsonify
+import pandas
 app = Flask(__name__)
+
+logging.basicConfig(
+    filename='API_OpenData.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',)
+
+logging.info("Démarrage de l'API")
+
+
+
 
 @app.route('/')
 def entry_point():
@@ -9,6 +22,10 @@ def entry_point():
 @app.route('/hello_world')
 def hello_world():
     return 'Hello World'
+
+@app.route('/city/stations/')
+def city_stations():
+
 
 if __name__ == '__main__':
     app.run(debug=True)
