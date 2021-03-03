108488b9ab9400ba52f229731ad245d7181d519e
Sarah <sarah.truffertgf2015@gmail.com>
2021-03-03 16:30:10 +0100

    test

--- BACK/fonctions.py
+++ BACK/fonctions.py
@@ -22,24 +22,22 @@ def city_station():
     station_list = set(db_tam['station'].tolist())
     return station_list
 
-# print(city_station())
+print(city_station())
 
 
-def prochain_transport():
-    """This function order the csv file to make some request, and answer
-    """
-    db_tam = voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv')
-    db_tam = db_tam.loc[db_tam['station'].isin(
-        [station])].sort_values(['heure_depart'], ascending=False)
-    resultat = {}
-    resultat["heure_depart"] = str(db_tam.iloc[0][8])
-    resultat["station"] = str(db_tam.iloc[0][3])
-    resultat["ligne"] = str(db_tam.iloc[0][4])
-    resultat["destination"] = str(db_tam.iloc[0][5])
-    return resultat
+# def prochain_transport():
+#     """This function order the csv file to make some request, and answer
+#     """
+#     db_tam = voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv')
+#     db_tam = db_tam.loc[db_tam['station'].isin([station])].sort_values(['heure_depart'], ascending=False)
+#     resultat = {}
+#     resultat["heure_depart"] = str(db_tam.iloc[0][8])
+#     resultat["station"] = str(db_tam.iloc[0][3])
+#     resultat["ligne"] = str(db_tam.iloc[0][4])
+#     resultat["destination"] = str(db_tam.iloc[0][5])
+#     return resultat
 
-print(prochain_transport)
-
+# print(prochain_transport())
 
 
 
