2321811697337171b5b1bd0765994503ed7b0fc3
SarahTruffert <77342348+SarahTruffert@users.noreply.github.com>
2021-03-04 21:03:18 +0100

    Update app.py
    Route 2 et 3 ok

--- BACK/app.py
+++ BACK/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 # , render_template
 import logging
-# from flask import Flask, abort, jsonify
-import fonctions
+#from flask import Flask, abort, jsonify
+from fonctions import prochain_transport
+from fonctions import depart_arrivee
 app = Flask(__name__)
+
 
 # @app.route('/')
 # def entry_point():
@@ -13,9 +15,21 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-@app.route('/city/stations/')
-def station():
-    return (city_station())
+#il manque la premiere route
+# @app.route('/city/<stations>')
+# def station():
+#     return jsonify(city_station())
+
+
+@app.route('/city/station/<stations>')
+def station(stations):
+    return jsonify(prochain_transport(stations))
+
+@app.route('/city/next/<station>')
+def next_station(station):
+    return jsonify(depart_arrivee(station))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
