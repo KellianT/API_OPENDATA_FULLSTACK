1e4ab61facce79ecd84f1205872d450980c3ece5
Sarah <sarah.truffertgf2015@gmail.com>
2021-03-04 20:24:44 +0100

    deuxieme route OK

--- BACK/app.py
+++ BACK/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 # , render_template
 import logging
-# from flask import Flask, abort, jsonify
-import fonctions
+#from flask import Flask, abort, jsonify
+from fonctions import prochain_transport, depart_arrivee
 app = Flask(__name__)
+
 
 # @app.route('/')
 # def entry_point():
@@ -13,9 +14,19 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-@app.route('/city/stations/')
-def station():
-    return (city_station())
+#pourquoi <> = comme une variable va changer
+@app.route('/city/station/<stations>')
+def station(stations):
+    return jsonify(prochain_transport(stations))
+
+@app.route('/city/next/<station>')
+def next_station(db_tam):
+    return jsonify(depart_arrivee(db_tam))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    #problèmes :
+    # 5 premières lignes
--- BACK/fonctions.py
+++ BACK/fonctions.py
@@ -1,19 +1,18 @@
 import pandas
 from flask import abort
-import logging
+# import logging
+
 
 def voir_csv(csv):
     """This function return the CSV file in Pandas data format
         Rename and delete unusual columns.
     """
-
     # logging.info("création de la base de données")
-    db_tam = pandas.read_csv(csv, sep =';',
+    db_tam = pandas.read_csv(csv, sep=';',
                                 header=1,
-                                usecols=[3,4,5,7],
-                                names=['station', 'ligne', 
-                                'direction','heure_depart'])
-                                                    
+                                usecols=[3, 4, 5, 7],
+                                names=['station', 'ligne',
+                                'direction', 'heure_depart'])                                                
     # db_tam = db_tam.rename(columns={
     #                     "stop_name":"station",
     #                     "route_short_name":"ligne",
@@ -21,7 +20,8 @@ def voir_csv(csv):
     #                     "departure_time":"heure_depart"},inplace=True)
     return db_tam
 
-# print(voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv')) 
+# print(voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv'))
+
 
 def city_station():
     """ This function displays all the stations.
@@ -37,12 +37,7 @@ def city_station():
 def prochain_transport(station):
     """This function order the csv file to make some request, and answer
         about the next transports.
-        Range le résultat dans un dictionnaire pour lire j.son
-        iloc = faire une boucle qui ajoute mon résultat par rapport à ce
-        que je veux récupérer comme valeur 
-        pour qu'il les ajoute dans mon dico.
     """
-
     db_tam = voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv')
     db_tam = db_tam.loc[db_tam['station'].isin([station])]
 
@@ -53,15 +48,27 @@ def prochain_transport(station):
         resultat["heure_depart"] = str(db_tam.iloc[i][3])
         resultat["station"] = str(db_tam.iloc[i][0])
         resultat["ligne"] = str(db_tam.iloc[i][1])
-        resultat["direction"] = str(db_tam.iloc[i][2])
         result.append(resultat)
-    
     return result
 
 # print(prochain_transport('GARE ST-ROCH T1'))
 
 
-# # def depart_arrivee():
-#     """This function order the csv file to make some request, and answer
-#         about the next transports for a destination given.
+
+def depart_arrivee(station):
+    # """This function order the csv file to make some request, and answer
+    #     about the next transports for a direction given.
     # """
+    db_tam = voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv')
+    db_tam = db_tam.loc[db_tam['station'].isin([station])]
+    result = []
+
+    for i in range(len(db_tam)):
+        resultat = {}
+        resultat["heure_depart"] = str(db_tam.iloc[i][3])
+        resultat["station"] = str(db_tam.iloc[i][0])
+        resultat["ligne"] = str(db_tam.iloc[i][1])
+        resultat["direction"] = str(db_tam.iloc[i][2])
+        result.append(resultat)
+    return result
+# print(depart_arrivee('GARE ST-ROCH T1'))
