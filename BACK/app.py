15779bb789291ea3098b8bdebc8cd839b35c6f61
Sarah <sarah.truffertgf2015@gmail.com>
2021-03-05 11:09:53 +0100

    chemin ok

--- BACK/app.py
+++ BACK/app.py
@@ -1,32 +1,37 @@
 from flask import Flask, jsonify
-# , render_template
+from flask import render_template
 import logging
-#from flask import Flask, abort, jsonify
-from fonctions import prochain_transport, depart_arrivee
+from flask import Flask, abort, jsonify
+from fonctions import prochain_transport
+from fonctions import depart_arrivee
+import sys
+sys.path.append('C:/Users/utilisateur/Desktop/API_OPENDATA_FULLSTACK/FRONT')
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def entry_point():
-#     return render_template('./app.html')
+@app.route('/')
+def entry_point():
+    return render_template('./app.html')
 
 @app.route('/hello_world')
 def hello_world():
-    return 'Hello World'
+   return jsonify('Hello, World!')
 
-#pourquoi <> = comme une variable va changer
+# #il manque la premiere route
+# @app.route('/city/<stations>')
+# def station():
+#     return jsonify(city_station())
+
+
 @app.route('/city/station/<stations>')
 def station(stations):
     return jsonify(prochain_transport(stations))
 
 @app.route('/city/next/<station>')
-def next_station(db_tam):
-    return jsonify(depart_arrivee(db_tam))
+def next_station(station):
+    return jsonify(depart_arrivee(station))
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    #problèmes :
-    # 5 premières lignes
