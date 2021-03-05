3829be6e27af3ddaeed647ea049c4cac5e091230
KellianT <kellianth@outlook.com>
2021-03-05 19:56:33 +0100

    avancement + modification fonctions

--- BACK/fonctions.py
+++ BACK/fonctions.py
@@ -1,47 +1,49 @@
 import pandas
+import logging
 from flask import abort
-# import logging
+import logging
 
 def voir_csv(csv):
+
+
     """This function return the CSV file in Pandas data format
-        Rename and delete unusual columns.
-    """
-    # logging.info("création de la base de données")
+        Rename and delete unusual columns. """
+
+        
+    logging.info("création de la base de données")
     db_tam = pandas.read_csv(csv, sep=';',
                                 header=1,
                                 usecols=[3, 4, 5, 7],
                                 names=['station', 'ligne',
                                 'direction', 'heure_depart'])                                                
-    # db_tam = db_tam.rename(columns={
-    #                     "stop_name":"station",
-    #                     "route_short_name":"ligne",
-    #                     "trip_headsign":"destination",
-    #                     "departure_time":"heure_depart"},inplace=True)
     return db_tam
 
 # print(voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv'))
 
 
-def city_station():
-    """ This function displays all the stations.
-    """
+def city_station(city):
+
+
+    """ This function displays all the stations. """
 
     db_tam = voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv')
     station_list = set(db_tam['station'].tolist())
     return station_list
 
-print(city_station())
+
+# print(city_station())
 
 
-def prochain_transport(station):
-    """This function order the csv file to make some request, and answer
-        about the next transports.
-    """
+def prochain_transport(station, db_tam):
+
+
+    """ This function order the csv file to make some request, and answer
+        about the next transports. """
+
+
     db_tam = voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv')
     db_tam = db_tam.loc[db_tam['station'].isin([station])]
-
     result = []
-
     for i in range(len(db_tam)):
         resultat = {}
         resultat["heure_depart"] = str(db_tam.iloc[i][3])
@@ -50,18 +52,20 @@ def prochain_transport(station):
         result.append(resultat)
     return result
 
+
 # print(prochain_transport('GARE ST-ROCH T1'))
 
 
-
 def depart_arrivee(station):
-    # """This function order the csv file to make some request, and answer
-    #     about the next transports for a direction given.
-    # """
+
+
+    """This function order the csv file to make some request, and answer
+        about the next transports for a direction given. """
+
+
     db_tam = voir_csv('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv')
     db_tam = db_tam.loc[db_tam['station'].isin([station])]
     result = []
-
     for i in range(len(db_tam)):
         resultat = {}
         resultat["heure_depart"] = str(db_tam.iloc[i][3])
@@ -70,4 +74,37 @@ def depart_arrivee(station):
         resultat["direction"] = str(db_tam.iloc[i][2])
         result.append(resultat)
     return result
+
+
 # print(depart_arrivee('GARE ST-ROCH T1'))
+
+
+def station_inall(station, db_tam):
+
+    """ This function checks if the stop name is in the database.
+    Parameters : a stop name (station), a database (db_tam) 
+    Returns : True or False 
+    Exemple : 'COMEDIE' returns True, 'COM' returns False """
+
+
+    logging.info(f"Vérifier si la station existe")
+    station = station.lower()
+    stations = list(set(db_tam['station'].to_list()))
+    if station in stations:
+        return True
+    else:
+        return False
+
+
+def ligne_inall(ligne, db_tam):
+
+    """ This function checks if the route short name is in the csv """
+
+
+    logging.info(f"Vérifier si la ligne existe")
+    lignes=list(set(db_tam['lignes'].to_list()))
+    if int(ligne) in lignes:
+        return True
+    else :
+        return False
+
