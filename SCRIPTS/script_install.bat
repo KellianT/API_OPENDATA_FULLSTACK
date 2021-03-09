a66ff862ac32e26d6e511cb58e7f90f5416065de
KellianT <kellianth@outlook.com>
2021-03-09 16:35:44 +0100

    Scripts OK !!! Pensez a changer vos logins et adresses afin de le connecter à vos serveurs respectifs ex: kellian@192.168.X.X ===> sarah@10.0.X.X et remplacer les chemins associés à vos fichiers

---
+++ SCRIPTS/script_install.bat
@@ -0,0 +1,27 @@
+echo "Création du dossier .ssh s'il n'existe pas déjà"
+ssh kellian@10.0.4.59 "mkdir .ssh"
+
+echo "Copier la clé public du poste vers le serveur"
+scp C:\\Users\utilisateur\.ssh\id_rsa.pub kellian@10.0.4.59:./.ssh/authorized_keys
+
+echo "Désactive l'authentification par mot de passe du SSH"
+ssh kellian@10.0.4.59 "sed 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config"
+
+echo "Importe les fichiers app.py, fonctions.py et app.html"
+scp  C:\Users\utilisateur\Desktop\Python\Projets\API_OPENDATA_FULLSTACK\BACK\app.py kellian@10.0.4.59:./app.py
+scp  C:\Users\utilisateur\Desktop\Python\Projets\API_OPENDATA_FULLSTACK\BACK\fonctions.py kellian@10.0.4.59:./fonctions.py
+scp -r C:\Users\utilisateur\Desktop\Python\Projets\API_OPENDATA_FULLSTACK\BACK\templates kellian@10.0.4.59:./
+
+clear
+
+
+echo "Déployer script.sh"
+scp  C:\Users\utilisateur\Desktop\Python\Projets\API_OPENDATA_FULLSTACK\SCRIPTS\scripts.sh kellian@10.0.4.59:./script.sh
+
+echo "Ouvrir les droits du script"
+ssh kellian@10.0.4.59 "chmod 777 ./script.sh"
+
+echo "Executer le script"
+ssh -t kellian@10.0.4.59 "sudo -S ./script.sh"
+
+clear
--- SCRIPTS/scripts.sh
+++ SCRIPTS/scripts.sh
@@ -1,1 +1,51 @@
-Remplissage pour GIT
+#!/bin/bash 
+echo "UBUNTU POST-INSTALL SCRIPT"
+apt -yq install net-tools 
+
+echo "Setting local date/time"
+timedatectl set-timezone Europe/Paris
+
+echo "Installing base packages"
+apt-get -yq install  git git-extras build-essential python3-pip htop glances
+
+echo "PermitRootLogin without-password" >> /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+clear 
+
+echo "Installation of the necessary modules"
+
+echo "Installing Pandas module" 
+sudo pip3 install pandas
+
+clear
+
+# echo "Creating a new directory for the Flask application and switching into it"
+# mkdir flask_app && cd flask_app
+
+echo "Installing Flask module"
+sudo pip3 install Flask
+
+echo "Installing Flask-CORS module"
+sudo pip3 install -U flask-cors
+
+clear
+
+echo "Updating APT..."
+apt-get update
+
+clear
+
+echo "Exporting the Flask app"
+export FLASK_APP=app.py
+
+echo "Running the Flask server"
+flask run --host=0.0.0.0 --port=80
+
+echo "End of script, enjoy your work ! "
+
+
+
+
+
+
